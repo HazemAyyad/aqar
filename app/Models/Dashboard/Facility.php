5eb31a1e9ac7fba8db1848bd73cba438e44af144
Hazem Ayad <hazem1fadil@gmail.com>
2024-09-29 12:50:43 +0300

    edit Facility

--- app/Models/Dashboard/Facility.php
+++ app/Models/Dashboard/Facility.php
@@ -5,13 +5,10 @@ namespace App\Models\Dashboard;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-<<<<<<< .mine
+
 use Spatie\Translatable\HasTranslations;
 
-=======
-use Spatie\Translatable\HasTranslations;
-use Spatie\Sluggable\HasTranslatableSlug;
->>>>>>> .theirs
+  
 class Facility extends Model
 {
     use HasFactory,SoftDeletes,HasTranslations;
